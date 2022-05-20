b5907ce6443080e6d4caaf417d7ceae9242dd40f
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-05-20 15:44:12 +0100

    userinfo stuff

--- client/src/container/Home.jsx
+++ client/src/container/Home.jsx
@@ -5,12 +5,25 @@ import{ Link, Route, Routes } from 'react-router-dom';
 
 import { Sidebar, UserProfile } from '../components/index';
 import Pins from './Pins';
+import { userQuery } from '../utils/data';
 import { client } from '../client';
 import logo from '../assets/logo.png';
 
 const Home = () => {
 
   const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear;
+  
+  useEffect(() => {
+    const query = userQuery(userinfo?.googleId);
+
+    client.fetch(query)
+      .then((data) => {
+        setUser(data[0]);
+      })
+  }, [])
+  
 
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
@@ -24,7 +37,7 @@ const Home = () => {
           <img src={logo} alt="logo" className='w-28'/>
         </Link>
         <Link to={'user-profile/${user?._id}'}>
-          <img src={logo} alt="logo" className='w-28'/>
+          <img src={user?.image} alt="logo" className='w-28'/>
         </Link>       
       </div>
     </div>
